691cafe6bec77ce6df1f6c4f70b3396da6018338
getrahulverma <98999706+getrahulverma@users.noreply.github.com>
2022-02-04 14:05:43 +0530

    Script for creating app user

--- usersetup.sql
+++ usersetup.sql
@@ -1,5 +1,7 @@
+-- Create test user & provide privileges
+-- DML Privileges has been given to this user under ddl.sql
+
 CREATE USER app_user IDENTIFIED BY app_user_pwd;
 
 GRANT CONNECT, RESOURCE TO app_user;
 
--- DML Privileges has been given to this user under ddl.sql
